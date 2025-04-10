8ab5103d65fb62cdd48fa83a012d917b3e9d1d6d
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2025-04-09 19:22:57 -0700

    Update login and register templates to use anchor tags for navigation; fix URL naming in user URLs

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,6 +4,6 @@ from .views import UserIndexView, UserRegisterView
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', UserIndexView.as_view(), name='index'),
+    path('login/', UserIndexView.as_view(), name='login'),
     path('register/', UserRegisterView.as_view(), name='register'),
 ]
