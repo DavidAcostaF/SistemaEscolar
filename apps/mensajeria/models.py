0ba8d349ac9ce5a82d750d17c235c6016cf2a2d4
AlanManjarrez <160210523+AlanManjarrez@users.noreply.github.com>
2025-05-12 19:15:50 -0700

    Nuevas vistas y API para mensajería
    feat(mensajeria): Refactorizar sistema de mensajería para incluir mensajes por curso
    
    feat(mensajeria): Implementar vista de lista de cursos para mensajería y actualizar plantillas
    
    feat(mensajeria): Agregar endpoints de API para recibir mensajes desde Moodle
    
    feat(mensajeria): Crear formularios y esquemas para la funcionalidad de mensajería
    
    fix(mensajeria): Mejorar manejo y visualización de mensajes en plantillas
    
    Co-Authored-By: JesusVillanuev <160086160+JesusVillanuev@users.noreply.github.com>
    Co-Authored-By: Alejandro Rayos <159945574+rsarayos@users.noreply.github.com>

--- apps/mensajeria/models.py
+++ apps/mensajeria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from apps.users.models import Alumno
+from apps.materias.models import Materia
 # Create your models here.
+class Mensaje(models.Model):
+    moodle_id_remitente = models.IntegerField()
+    moodle_id_destinatario = models.IntegerField()
+    curso_moodle_id = models.IntegerField()
+    contenido = models.TextField()
+    enviado_por_django = models.BooleanField(default=False)
+    timestamp = models.DateTimeField()
+    recibido = models.DateTimeField(auto_now_add=True)
+
--- apps/mensajeria/urls.py
+++ apps/mensajeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MensajeriaView
+from .views import ChatCursoView,ListaCursosAlumnoView
 
 app_name = 'mensajeria'
 
 urlpatterns = [
-    path('mensajeria/', MensajeriaView.as_view(), name='mensajeria'),
+    path('mensajeria/', ListaCursosAlumnoView.as_view(), name='lista_cursos'),
+    path('enviar-mensaje/<int:curso_id>/', ChatCursoView.as_view(), name='enviar_mensaje'),
 ]
